2b7d51a2892e52a094eec5c6fa04bb5d1471a394
Ou <agnaw1022@gmail.com>
2020-09-08 12:06:58 -0600

    update

---
+++ app/mobility.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('mobility', __name__, url_prefix='/mobility')
+
+@bp.route('/air')
+def air_traffic():
+    return 'haha'
+
+@bp.route('/city')
+def city_congestion():
+    pass
+
+@bp.route('/border')
+def border_wait():
+    pass
+
+@bp.route('/gas')
+def fillup():
+    pass
